be9fc76b81f116ca008b2faaa4ccaeddfadeb9f2
JoseBruno2005 <josebrunobispoparnaiba@gmail.com>
2024-09-20 18:30:22 -0300

    Curso Service

---
+++ src/main/java/service/CursoService.java
@@ -0,0 +1,18 @@
+package service;
+
+import dao.CursoDAO;
+import domain.Curso;
+
+public class CursoService {
+
+    private CursoDAO cursoDAO;
+    private Curso curso;
+
+    public CursoService(){
+        curso = new Curso();
+    }
+
+    public void save(Curso curso) {
+        cursoDAO.save(curso);
+    }
+}
